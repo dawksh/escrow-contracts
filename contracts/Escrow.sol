7948c2bba55dc1d178ee430149cad27e0d782723
Daksh Kulshrestha <20741933+dawksh@users.noreply.github.com>
2022-07-18 20:59:31 +0530

    feat: prototype for 721 standard done

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/tokens/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/tokens/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
 /// @title Escrow721
 /// @author Daksh Kulshrestha
@@ -12,27 +12,54 @@ contract Escrow721 is IERC721Receiver {
     address public immutable seller;
     address public immutable buyer;
     uint256 public deadline;
-    uint256 tokenID;
-    uint256 amount;
-    IERC721 tokenContract;
+    uint256 public tokenID;
+    uint256 public amount;
+    IERC721 public tokenContract;
+    bool isLive;
 
-    constructor(
-        address _seller,
-        address _buyer,
-    ) {
+    constructor(address _seller, address _buyer) {
         seller = _seller;
         buyer = _buyer;
-        deadline = _deadline;
-        tokenID = _tokenID;
     }
 
-    function initialize(address contractAddress, uint _tokenID, uint _amount) external {
+    function initialize(
+        address contractAddress,
+        uint256 _tokenID,
+        uint256 _amount,
+        uint256 _deadline
+    ) external {
         require(msg.sender == seller, "Error: Only seller can initialize");
         tokenContract = IERC721(contractAddress);
-        bool s = tokenContract.transferFrom(seller, address(this), _tokenID);
-        require(s, "Token Transfer Unsuccessful");
+        tokenContract.transferFrom(seller, address(this), _tokenID);
+        deadline = _deadline;
         amount = _amount;
         tokenID = _tokenID;
     }
 
+    function pay() external payable {
+        require(isLive, "Escrow not active");
+        require(msg.sender == buyer, "Caller not the Buyer");
+        require(msg.value == amount, "Not enough funds");
+        tokenContract.transferFrom(address(this), buyer, tokenID);
+        payable(seller).transfer(msg.value);
+    }
+
+    function cancelEscrow() external {
+        require(block.timestamp > deadline, "Deadline not crossed");
+        isLive = false;
+        tokenContract.transferFrom(address(this), seller, tokenID);
+    }
+
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _tokenId,
+        bytes calldata _data
+    ) external pure override returns (bytes4) {
+        _operator;
+        _from;
+        _tokenId;
+        _data;
+        return 0x150b7a02;
+    }
 }
